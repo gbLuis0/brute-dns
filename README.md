1049fa6352568846f48a71689b6b4da6bed41809
Spyware <gabrielluisdm13@gmail.com>
2022-03-13 16:49:32 -0300

    Up main, readme and wordlist

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# brute-dns
- Brute force de subdomínios em python
+![](https://camo.githubusercontent.com/71b837571c48af3aa60a73dbc9d5936aa359d78efbfa8a6743cbbbc16b80ef4d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3830353930323039333930363630383138362f3830353931333937323533353539303932322f74656e6f722e676966)
+
+# ***brute-dns***
+
+Um Brute force de DNS feito em python
+
+> Disponível em qualquer shell ✅<br/>
+> Requisitos: Git, Python3
+
+## Inicialização 💻 (Linux)
+```bash
+apt-get update
+apt-get upgrade -y
+apt-get install git python -y
+```
+
+## Acesso 📂
+```bash
+cd brute-dns
+python main.py
+```
+
+Se quiser elogiar, criticar e etc, me chame no [Whatsapp](http://wa.me/559885267746)!
+
+![](https://camo.githubusercontent.com/71b837571c48af3aa60a73dbc9d5936aa359d78efbfa8a6743cbbbc16b80ef4d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f3830353930323039333930363630383138362f3830353931333937323533353539303932322f74656e6f722e676966)
--- main.py
+++ main.py
@@ -1,30 +1,38 @@
-from os import system, name
+from os import system, name, getcwd
 try:
     import dns.resolver
+    from pyfiglet import Figlet
 except:
-    system('pip install dnspython')
+    system('pip install dnspython pyfiglet')
 finally:
     import dns.resolver
+    from pyfiglet import Figlet
+
+def cls():
+    system('cls' if name == 'nt' else 'clear')
 
 f = '\033[m'
 verm = '\033[31;1m'
 verd = '\033[32;1m'
-
-
 dns = dns.resolver.Resolver()
+banner = Figlet('slant').renderText('brute-dns')
 
-domain = input('Digite o domínio: ').strip().lower()
-
-cam = input('caminho da worldlist: ').strip()
-subdomains = open(cam, 'r').read().strip().splitlines()
-
-per = 's'
-while per == 's':
+per = 'y'
+while per == 'y':
+    cls()
+    print('\033[34;1m'+banner+f)
+    domain = input('Digite o domínio: ').strip().lower()
+    cam = input('caminho da wordlist: (enter para inserir a wordlist da pasta) ').strip()
+    if cam == '':
+        cam = getcwd() + 'wordlist.txt'
+    subdomains = open(cam, 'r').read().strip().splitlines()
     for s in subdomains:
         try:
             dom = s + '.' + domain
             res = dns.resolve(dom, 'A')
             for i in res:
-                print(f'{dom} -> \033[32;1m{i}\033[m') 
+                print(f'{dom} -> {verd}{i}{f}') 
         except:
-            print(dom, '\033[31;1mnot found\033[m')
+            print(dom, verm + 'not found' + f + ' '*50, end='\r')
+    per = input('Deseja voltar? [y/n] ').strip().lower()[0]
+cls()
