6e38f5a1c7f3cc49a8b4085fbde3bf96fe4b89cd
Spyware <gabrielluisdm13@gmail.com>
2022-03-13 14:54:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ except:
 finally:
     import dns.resolver
 
+f = '\033[m'
+verm = '\033[31;1m'
+verd = '\033[32;1m'
+
 
 dns = dns.resolver.Resolver()
 
 domain = input('Digite o domínio: ').strip().lower()
 
-subd = input('caminho da wor')
+cam = input('caminho da worldlist: ').strip()
+subdomains = open(cam, 'r').read().strip().splitlines()
+
+per = 's'
+while per == 's':
+    for s in subdomains:
+        try:
+            dom = s + '.' + domain
+            res = dns.resolve(dom, 'A')
+            for i in res:
+                print(f'{dom} -> \033[32;1m{i}\033[m') 
+        except:
+            print(dom, '\033[31;1mnot found\033[m')
